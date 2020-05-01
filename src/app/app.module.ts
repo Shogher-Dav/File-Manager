3b8944381d96a7de0e6d0963ee781916cb1ec9e3
Shogher Davtyan <shogher.davtyan95@gmail.com>
2020-05-01 18:13:20 +0400

    add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { FileManagerComponent } from './file-manager/file-manager.component';
+import { FileManagerComponent} from './file-manager/file-manager.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
--- src/app/file-manager/file-manager.component.ts
+++ src/app/file-manager/file-manager.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, Renderer, ElementRef,
+    HostListener, AfterViewChecked } from '@angular/core';
 import { FileService } from '../core/services/file.service';
 import { Observable } from 'rxjs';
 import { FileElement } from '../core/interfaces/element';
@@ -8,18 +9,31 @@ import { FileElement } from '../core/interfaces/element';
   templateUrl: './file-manager.component.html',
   styleUrls: ['./file-manager.component.scss']
 })
-export class FileManagerComponent implements OnInit {
+
+export class FileManagerComponent implements OnInit, AfterViewChecked {
 
     public fileElements: Observable<FileElement[]>;
     public canNavigateUp = false;
     public currentRoot: FileElement;
     public currentPath: string;
     public searchedName: string;
-    constructor(private fileService: FileService) { }
+    public selectedELements = {};
+
+    @ViewChildren('divConteiner') divConteiner;
+    public index = 0;
+    public arrElements = [];
+
+    constructor(private fileService: FileService,
+        private renderer: Renderer) { }
 
     ngOnInit() {
         this.updateFileElementArray();
     }
+
+    ngAfterViewChecked() {
+        this.arrElements = this.divConteiner.toArray();
+
+      }
 
     updateFileElementArray() {
         this.fileElements = this.fileService.itemsInFolder(this.currentRoot ? this.currentRoot.name.toLowerCase() : 'root');
@@ -30,6 +44,7 @@ export class FileManagerComponent implements OnInit {
         this.updateFileElementArray();
         this.currentPath = this.currentRoot.path;
         this.canNavigateUp = true;
+        this.index = 0;
     }
 
     navigateToBack() {
@@ -42,6 +57,8 @@ export class FileManagerComponent implements OnInit {
         } else {
             this.fileElements = this.fileService.itemsInFolder(parentName.toLowerCase());
         }
+        this.index = 0;
+
     }
 
     popFromPath(path: string) {
@@ -75,4 +92,47 @@ export class FileManagerComponent implements OnInit {
     sortByModificationDateReverce() {
         this.fileElements = this.fileService.sortModifiedDateReverce();
     }
+
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.keyCode === 38) {
+        if (this.index >= this.arrElements.length) {
+            this.index--;
+        }
+
+        if (this.index > 0) {
+            this.removeBorderStyle(this.index);
+            this.index--;
+            if (this.index < 0) {
+                console.log('poqr e 0-ic');
+            } else {
+                this.setBorderStyle(this.index);
+            }
+        }
+    }
+
+    if (event.keyCode === 40) {
+        if (this.index === 0) {
+        this.setBorderStyle(0);
+        this.index++;
+
+        } else if (this.index > 0 && this.index < this.arrElements.length) {
+            this.removeBorderStyle(this.index - 1);
+            this.setBorderStyle(this.index);
+            this.index++;
+        }
+    }
+  }
+
+   setBorderStyle(index) {
+        const element = this.arrElements[index].nativeElement;
+        this.renderer.setElementStyle(element,  'border', '1px solid blue ');
+
+   }
+   removeBorderStyle(index) {
+
+    const element = this.arrElements[index].nativeElement;
+    this.renderer.setElementStyle(element, 'border', 'none');
+   }
 }
